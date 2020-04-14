b918b1b78923023ed7f7f3013efa4850e2faf8bd
MegaDr <megadwiranirodja@gmail.com>
2020-04-14 11:23:27 +0700

    Responsi Pemrograman

--- Karyawan.cs
+++ Karyawan.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ResponsiPemrograman2723
+namespace ResponsiPemrograman
 {
     class Karyawan
     {
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ResponsiPemrograman2723
+namespace ResponsiPemrograman
 {
     
 
@@ -13,9 +13,9 @@ namespace ResponsiPemrograman2723
         static void Main(string[] args)
         {
            Console.WriteLine("NIK\tNama\t\t\tGaji Bulanan\n------------------------------");
-           Karyawan karyawan = new Karyawan("12345", "Mega", 100000);
+           Karyawan karyawan = new Karyawan("12345", "ana", 1000000);
            karyawan.getKaryawan();
-           Karyawan karyawan2 = new Karyawan("678910", "Dwirani", 150000);
+           Karyawan karyawan2 = new Karyawan("678910", "ani", 1500000);
            karyawan2.getKaryawan();
             Console.WriteLine("\n");
             Console.WriteLine("Asyiiiiikkkk Naik Gaji 10%");
